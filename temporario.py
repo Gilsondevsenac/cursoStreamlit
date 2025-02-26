bad23cc2e7020f9ed3a61446ad57c366c05e1453
Gilsondevsenac <carvalho98433@gmail.com>
2025-02-26 19:53:56 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.header("cabeçalho")
 st.toggle("botão salvar")
 st.text_area("Enter text")
-st.text_input(Movie title", "Life of Gilson")
+st.text_input("Movie title", "Life of Gilson")
 st.button("Botão Salvar")
