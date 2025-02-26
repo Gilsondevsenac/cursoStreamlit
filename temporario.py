71cbc9f2b368139158448d85f03ea3a478f38f7a
Gilsondevsenac <carvalho98433@gmail.com>
2025-02-26 20:05:43 -0300

    Update temporario.py
    Adicionei o st.multiselect

--- temporario.py
+++ temporario.py
@@ -4,5 +4,8 @@ st.toggle("botão salvar")
 st.text_area("Enter text")
 st.text_input("Movie title", "Life of Gilson")
 st.selectbox("Qual a sua cor favorita?", ("Azul", "Vermelho","Verde","Azul"))
-
+st.multiselect(
+  "Quais são sua cores favoritas?",
+  ["Verde", ""Vermelho","Azul"],
+  ["Amarelo", "Vermelho"])
 st.button("Botão Salvar")
