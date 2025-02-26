fff828570b49453d4bd2a578fddf1580b874f2da
Gilsondevsenac <carvalho98433@gmail.com>
2025-02-26 20:11:33 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.header("cabeçalho")
 st.toggle("botão salvar")
 st.text_area("Enter text")
-st.multiselect(
+st.multiselect (
   "Quais são suas cores favoritas?",
   ["Verde","Vermelho","Azul"],
   ["Amarelo","Vermelho"])
