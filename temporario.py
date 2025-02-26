0a0f42996139d4e1376fb83b1eb58a6a1b8d5c24
Gilsondevsenac <carvalho98433@gmail.com>
2025-02-26 20:29:03 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.header("cabeçalho")
 st.toggle("botão salvar")
 st.text_area("Enter text")
@@ -8,4 +9,12 @@ st.selectbox("Qual a sua cor favorita?", ("Azul", "Vermelho","Verde","Azul"))
 st.button("Botão Salvar")
 st.color_picker("Pick A Color", "#00f900")
 st.feedback("stars")
+df = pd.Dataframe(
+  [
+    {"command":"st.slectbox","rating":4,"is_winget":True},
+    {"command":"st.slectbox","rating":5,"is_winget":False},
+    {"command":"st.slectbox","rating":3,"is_winget":True},
+  ]
+)
 
+edited_df=st.data_editor(df)
