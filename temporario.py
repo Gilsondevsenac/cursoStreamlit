1d715ccbec3aab76e4d928c412c911c5c2abbcf5
Gilsondevsenac <carvalho98433@gmail.com>
2025-02-26 20:12:06 -0300

    Update temporario.py
    retirei multiselect

--- temporario.py
+++ temporario.py
@@ -2,10 +2,6 @@ import streamlit as st
 st.header("cabeçalho")
 st.toggle("botão salvar")
 st.text_area("Enter text")
-st.multiselect (
-  "Quais são suas cores favoritas?",
-  ["Verde","Vermelho","Azul"],
-  ["Amarelo","Vermelho"])
 st.text_input("Movie title", "Life of Gilson")
 st.selectbox("Qual a sua cor favorita?", ("Azul", "Vermelho","Verde","Azul"))
 
