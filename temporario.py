0a1c1483f4705b33e04e90294f3b3c7f13c7fe48
Gilsondevsenac <carvalho98433@gmail.com>
2025-02-26 19:58:30 -0300

    Update temporario.py
    Adicionei st.selectbox

--- temporario.py
+++ temporario.py
@@ -3,4 +3,6 @@ st.header("cabeçalho")
 st.toggle("botão salvar")
 st.text_area("Enter text")
 st.text_input("Movie title", "Life of Gilson")
+st.selectbox("Qual a sua cor favorita?", ("Azul", "Vermelho""Vermelho"))
+
 st.button("Botão Salvar")
