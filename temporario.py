2f237e5abdcf3bc7946e97e6ef409ec26e68fd35
Gilsondevsenac <carvalho98433@gmail.com>
2025-02-26 19:59:17 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -3,6 +3,6 @@ st.header("cabeçalho")
 st.toggle("botão salvar")
 st.text_area("Enter text")
 st.text_input("Movie title", "Life of Gilson")
-st.selectbox("Qual a sua cor favorita?", ("Azul", "Vermelho""Vermelho"))
+st.selectbox("Qual a sua cor favorita?", ("Azul", "Vermelho","Verde","Azul"))
 
 st.button("Botão Salvar")
