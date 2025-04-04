eedaee0f00c09af0eb2cbe91b4f6c58a6387240f
Gilsondevsenac <carvalho98433@gmail.com>
2025-04-04 20:35:10 -0300

    Update Aula lista 01.py

--- Aula lista 01.py
+++ Aula lista 01.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-xicarasfarinha  = float(st.text_input("Insira a quantidade de xicaras de farinha\n"))
-gramasfarinha = float(st.text_input("Insira a quantidade de gramas de farinha\n"))
-totalfarinha = xicarasfarinha*gramasfarinha
+xicarasfarinha  = float(st.text_input("Insira a quantidade de xicaras de farinha"))
+gramasfarinha = float(st.text_input("Insira a quantidade de gramas de farinha"))
+# totalfarinha = xicarasfarinha*gramasfarinha
